6f2cc4210b83891224daa2cccfa0d820e1b4ff56
Sam-Browder <browdess@rose-hulman.edu>
2015-01-23 12:26:01 -0500

    added java project

---
+++ PoeTree/src/PathFinding/AppMain.java
@@ -0,0 +1,13 @@
+package PathFinding;
+
+public class AppMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello, world");
+
+	}
+
+}
---
+++ PoeTree/src/PathFinding/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Sam Browder
+ *
+ */
+package PathFinding;
